847b8ea08e3e25b8062210883462e15cab0ddd1b
fpaniagua <fpaniagua.ifct0609.2020@gmail.com>
2020-02-13 14:03:36 +0100

    Initial commit

---
+++ src/es/cice/ejercicio1/Ejecutador.java
@@ -0,0 +1,14 @@
+package es.cice.ejercicio1;
+
+/**
+ * Esta es la primera clase de Java de <strong>mi vida</strong>
+ * @author Fernando Paniagua
+ * @since 1.0
+ */
+public class Ejecutador {
+    //psvm + tab
+    public static void main(String[] args) {
+        //sout + tab
+        System.out.println("Hola mundo");
+    }
+}
